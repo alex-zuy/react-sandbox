15f6734f14844b2e6d904918a1bf57f89f0924a2
Alexey Zuy <zuy.alexey@gmail.com>
2018-11-08 17:05:14 +0200

    Fixed tests setup

--- app/components/Multiselect.jsx
+++ app/components/Multiselect.jsx
@@ -1,5 +1,5 @@
 import React, {Component, PropTypes} from 'react';
-import _ from 'lodash-compat';
+import _ from 'lodash';
 import JQueryUIComponent from './JQueryUIComponent.jsx';
 
 const EMPTY_OPTION_VALUE = '';
